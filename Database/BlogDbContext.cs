13f86947ef98b4112682aca85f4f7b13c1f37982
Franklin Reyes <frankmreyeshn96@gmail.com>
2021-09-01 01:41:58 -0600

    Database context and connection string added.

---
+++ Database/BlogDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace my_app_backend.Database
+{
+    public class BlogDbContext: DbContext
+    {
+        public BlogDbContext(DbContextOptions<BlogDbContext> options) : base(options)
+        {
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
